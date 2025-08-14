e1966e820e7b847b805862a19405f210107f57d8
Tim Spinney <tspinney@seattleu.edu>
2025-08-14 11:52:47 -0700

    additional smoke tests

--- test/compilers/cfloor1/smoke_test.dart
+++ test/compilers/cfloor1/smoke_test.dart
@@ -16,4 +16,29 @@ main() {
     expect(instructionGenerator.instructions, isNotEmpty);
     expect(errorCollector.errors, isEmpty);
   });
+
+  test('produces errors for invalid syntax', () {
+    const source = '''
+    int a = 4
+    a = a * a;
+    write(a);
+    ''';
+
+    final errorCollector = SyntaxErrorCollector();
+    compileCFloor1(source, errorCollector);
+    expect(errorCollector.errors, isNotEmpty);
+  });
+
+  test('produces errors for duplicate variable names', () {
+    const source = '''
+    int a = 4;
+    int a = 5;
+    write(a);
+    ''';
+
+    final errorCollector = SyntaxErrorCollector();
+    final instructionGenerator = compileCFloor1(source, errorCollector);
+    expect(instructionGenerator.semanticErrorCollector.errors, isNotEmpty);
+    print(instructionGenerator.semanticErrorCollector.errors);
+  });
 }
